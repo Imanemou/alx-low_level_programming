3ff1061855bb35e883fbe57764375235f9a6aee9
Imanemou <imane.moustakimi1998@gmail.com>
2023-05-21 01:44:24 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <dtdlib.h>
+#include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -10,7 +9,7 @@
 int main(void)
 
 {
-
+i
 	char low;
 
 	for (low = 'z'; low >= 'a'; low--)
